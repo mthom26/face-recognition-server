d4a5289edc6e1ee1c6a92946ddf52793862d3b18
mthom26 <mthom2626@gmail.com>
2019-02-01 23:21:57 +0000

    Add about route

--- src/server.js
+++ src/server.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/about', (req, res) => {
+  res.json({message:"About"})
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on PORT: ${PORT}`);
 });
