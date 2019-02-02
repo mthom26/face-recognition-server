7cb319dee8e88c86c8022821317c8ad9e6c1b409
mthom26 <mthom2626@gmail.com>
2019-02-02 11:48:27 +0000

    Add Database
    Add 'mongoose' and 'dotenv' and setup initial database connection
    with simple User model.

---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.set('debug', true);
+
+mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true }, err => {
+  if (err) {
+    console.log(`Could not connect to Database: ${process.env.DATABASE_URI}`);
+  } else {
+    console.log(`Connected to Database: ${process.env.DATABASE_URI}`);
+  }
+});
+
+module.exports.User = require('./user');
