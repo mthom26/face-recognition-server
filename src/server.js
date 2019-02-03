e3c17b49c691794a2773337688801b4cc9676cce
mthom26 <mthom2626@gmail.com>
2019-02-03 20:22:34 +0000

    Update clearDb to seed database with some initial data

--- src/server.js
+++ src/server.js
@@ -6,15 +6,32 @@ const cors = require('cors');
 const db = require('./models');
 const authRoutes = require('./routes/auth');
 
-const clearDb = async () => {
-  console.log('clearing db');
+const seedDb = async () => {
+  console.log('seeding db');
   await db.User.deleteMany({});
+  await db.User.create([
+    {
+      name: 'Matthew',
+      email: 'matt@testmail.com',
+      password: 'matt123'
+    },
+    {
+      name: 'Jimmy',
+      email: 'jimmy@testmail.com',
+      password: 'muhpassword'
+    },
+    {
+      name: 'Datboi',
+      email: 'datboi@testmail.com',
+      password: 'datboi'
+    }
+  ]);
 };
 
 const isTest = process.env.TEST_DB;
 if (isTest) {
   // clear database for testing
-  clearDb();
+  seedDb();
 }
 
 const PORT = process.env.PORT || 8000;
