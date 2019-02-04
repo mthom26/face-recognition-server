b4ef93af237e01b842e5adb0c86caa30a9d01ca5
mthom26 <mthom2626@gmail.com>
2019-02-04 18:22:23 +0000

    Add login controller and route

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -26,4 +26,29 @@ const postSignUp = async (req, res) => {
   }
 };
 
-module.exports = { postSignUp };
+const postLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await db.User.findOne({ email: email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: 'That email/password is incorrect.' });
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      return res
+        .status(400)
+        .json({ message: 'That email/password is incorrect.' });
+    }
+
+    res.status(200).json({ message: 'Logged In!' });
+  } catch (err) {
+    res.status(500).json({ error: 'Sorry something went wrong.' });
+    console.log(err);
+  }
+};
+
+module.exports = { postSignUp, postLogin };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const { postSignUp } = require('../controllers/auth');
+const { postSignUp, postLogin } = require('../controllers/auth');
 
 const router = express.Router();
 
 router.post('/signup', postSignUp);
+router.post('/login', postLogin);
 
 module.exports = router;
