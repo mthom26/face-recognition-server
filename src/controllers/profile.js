0afb597f311e602097d4dcaf0d1aec4260e015df
mthom26 <mthom2626@gmail.com>
2019-03-04 12:24:28 +0000

    Add update name route and controller

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -23,4 +23,24 @@ const getProfile = async (req, res) => {
   }
 };
 
-module.exports = { getProfile };
+const updateName = async (req, res) => {
+  try {
+    const { newName } = req.body;
+    const { user } = res.locals;
+    if (!user) {
+      return res.status(500).json({ error: 'Sorry something went wrong.' });
+    }
+    user.name = newName;
+    await user.save();
+
+    res.status(200).json({
+      message: 'Updated Name',
+      data: user.name
+    });
+  } catch (err) {
+    res.status(500).json({ error: 'Sorry something went wrong.' });
+    console.log(err);
+  }
+};
+
+module.exports = { getProfile, updateName };
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { getProfile } = require('../controllers/profile');
+const { getProfile, updateName } = require('../controllers/profile');
 const { isAuthenticated } = require('../middleware/isAuthenticated');
 
 const router = express.Router();
 
 router.post('/profile', isAuthenticated, getProfile);
+router.post('/update-name', isAuthenticated, updateName);
 
 module.exports = router;
