9fb8c41c4decb10dc6d71c51bdfd8acc1ce12a6b
mthom26 <mthom2626@gmail.com>
2019-03-26 23:42:17 +0000

    Add password update route and controller

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 const getProfile = async (req, res) => {
   try {
     const { user } = res.locals;
@@ -43,4 +45,28 @@ const updateName = async (req, res) => {
   }
 };
 
-module.exports = { getProfile, updateName };
+const updatePassword = async (req, res) => {
+  try {
+    const { newPassword, oldPassword } = req.body;
+    const { user } = res.locals;
+    if (!user) {
+      return res.status(500).json({ error: 'Sorry something went wrong.' });
+    }
+
+    const isValid = await bcrypt.compare(oldPassword, user.password);
+    if (!isValid) {
+      return res.status(400).json({ message: 'That password is incorrect.' });
+    }
+
+    const hashed = await bcrypt.hash(newPassword, 10);
+    user.password = hashed;
+    await user.save();
+
+    res.status(200).json({ message: 'Updated Password' });
+  } catch (err) {
+    res.status(500).json({ error: 'Sorry something went wrong.' });
+    console.log(err);
+  }
+};
+
+module.exports = { getProfile, updateName, updatePassword };
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
-const { getProfile, updateName } = require('../controllers/profile');
+const {
+  getProfile,
+  updateName,
+  updatePassword
+} = require('../controllers/profile');
 const { isAuthenticated } = require('../middleware/isAuthenticated');
 
 const router = express.Router();
 
 router.post('/profile', isAuthenticated, getProfile);
 router.post('/update-name', isAuthenticated, updateName);
+router.post('/update-password', isAuthenticated, updatePassword);
 
 module.exports = router;
